59992c8bed7bf9a065be641cf9695677e26eee92
SN00R <2097noor@gmail.com>
2023-06-22 14:48:09 +0200

    Add Mean to plots

--- plotting.py
+++ plotting.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+import numpy as np
+from math import sqrt
 
-df1 = pd.read_csv('/Users/noor/Bot4BACS/cal2/F3_B8_07_6C_28_8E.csv')    #Window
-df2 = pd.read_csv('/Users/noor/Bot4BACS/cal2/C3_23_CF_79_6C_FB.csv')    #Coffee Desk
-df3 = pd.read_csv('/Users/noor/Bot4BACS/cal2/DD_3F_BC_9B_56_EC.csv')    #Meeting Table
-df4 = pd.read_csv('/Users/noor/Bot4BACS/cal2/E9_FC_42_4B_FB_7C.csv')    #Robot
-#df5 = pd.read_csv('/Users/noor/Bot4BACS/cal1/FE_ED_E9_B2_20_8E.csv')    #Not in Flight Arena -> Secret
+df1 = pd.read_csv('/Users/noor/Bot4BACS/cal1/F3_B8_07_6C_28_8E.csv')    #Window
+df2 = pd.read_csv('/Users/noor/Bot4BACS/cal1/C3_23_CF_79_6C_FB.csv')    #Coffee Desk
+df3 = pd.read_csv('/Users/noor/Bot4BACS/cal1/DD_3F_BC_9B_56_EC.csv')    #Meeting Table
+df4 = pd.read_csv('/Users/noor/Bot4BACS/cal1/E9_FC_42_4B_FB_7C.csv')    #Robot
+#df5 = pd.read_csv('/Users/noor/Bot4BACS/cal1/FE_ED_E9_B2_20_8E.csv')   #Not in Flight Arena -> Secret
 
 helpertime=[]
 helperparam=[]
@@ -22,27 +25,35 @@ for x in values:
         #helperparam = df["CO2"]
         helperparam = df[x]
         param[i] = helperparam
-        print(x)
+    param = param.dropna()
+    time = time.dropna()
+    time[4] = time.mean(axis=1)
+    param[4] = param.iloc[:,0:4].mean(axis=1)           # Add mean column
+    param[4] = round(param[4],1)
+    print(param)    
+    # Root Mean Square Error
+    #mse_1 = mean_squared_error(param[5])
+    #rmse1 = sqrt(mse_1) 
     plt.title(str(x)+' Measurements on 21.06.2023 for calibration')
     if x == "CO2":
         plt.ylabel('CO2 in ppm')
-        plt.plot(time,param,label=["Window","Coffee","Meeting","Robot"])
+        plt.plot(time,param,label=["F3","C3","DD","E9","Mean"])
     elif x == "Temperature":
         plt.ylabel('Temperature in °C')
-        plt.plot(time,param/100,label=["Window","Coffee","Meeting","Robot"])
+        plt.plot(time,param/100,label=["F3","C3","DD","E9","Mean"])
     elif x == "Humidity":
         plt.ylabel('Humidity in %RC')
-        plt.plot(time,param/100,label=["Window","Coffee","Meeting","Robot"])
+        plt.plot(time,param/100,label=["F3","C3","DD","E9","Mean"])
     elif x == "Pressure":
         plt.ylabel('Pressure in Pa')
-        plt.plot(time,param/100,label=["Window","Coffee","Meeting","Robot"])
+        plt.plot(time,param/100,label=["F3","C3","DD","E9","Mean"])
     elif x == "Photo":
         plt.ylabel('ADC')
-        plt.plot(time,param,label=["Window","Coffee","Meeting","Robot"])
+        plt.plot(time,param,label=["F3","C3","DD","E9","Mean"])
     plt.legend()    
     plt.xlabel('Time')
     #plt.xticks(rotation= 30, ha='right')
     plt.grid(alpha=0.25)
-    plt.savefig('/Users/noor/Bot4BACS/cal2/'+str(x)+'.png',bbox_inches='tight', dpi=200)
+    plt.savefig('/Users/noor/Bot4BACS/cal1/'+str(x)+'.png',bbox_inches='tight', dpi=200)
     plt.show()
 
