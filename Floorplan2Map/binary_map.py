0c95698851a613faba2a1003dd825e83e47fac20
SN00R <2097noor@gmail.com>
2023-07-18 21:14:03 +0200

    Create Map from Floorplan

---
+++ Floorplan2Map/binary_map.py
@@ -0,0 +1,15 @@
+import cv2 # Import OpenCV
+   
+# read the image file
+img = cv2.imread('/Users/noor/Bot4BACS/Floorplan2Map/VisionWood_BW_binary_2.png')
+   
+ret, bw_img = cv2.threshold(img, 220, 255, cv2.THRESH_BINARY)
+   
+# converting to its binary form
+bw = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
+  
+# Display and save image 
+cv2.imshow("Binary", bw_img)
+cv2.imwrite("VisionWood_BW_binary_3.png", bw_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ playground.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/local/python3
+import numpy as np
+import cv2 as cv
+
+#Mouse callback function
+def draw_shape(event,x,y,flags,param):
+    print("event : ",event)
+    if event == cv.EVENT_FLAG_LBUTTON:
+        cv.circle(img,(x,y),100,(255,0,0),-1)
+
+#Create a black image, a window and bind the function to the window
+img = np.zeros((780,780,3),np.uint8)
+cv.namedWindow('DrawWithMouse')
+cv.setMouseCallback('DrawWithMouse',draw_shape)
+
+while(1):
+    cv.imshow('DrawWithMouse',img)
+    if cv.waitKey(10) & 0xFF == 27: #ANDing with 0xFF as my machine is 64 bit
+        break
+
+cv.destroyWindow('DrawWithMouse')
