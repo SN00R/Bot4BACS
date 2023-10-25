b5e7052ab34bb46ecf6d9e883f2372cedf5351c2
SN00R <2097noor@gmail.com>
2023-10-25 11:55:22 +0200

    Save selected Images

--- ObjectDetection/Renamer.py
+++ ObjectDetection/Renamer.py
@@ -1,13 +1,22 @@
 import os
 
-dirname = "/Users/noor/Bot4BACS/ObjectDetection/Img"
+dirname = "/Users/noor/Bot4BACS/ObjectDetection/Img/"
 imnum = 1
 for img in os.listdir(dirname):
-    imgpath = os.path.join(dirname,img) 
+    #imgpath = os.path.join(dirname,img) 
+    imgpath = dirname + img 
     filename = "Img_" + str(imnum) + '.jpg'
-    savepath = os.path.join(dirname, filename)
+    #savepath = os.path.join(dirname, filename)
+    savepath = dirname + filename
     os.rename(imgpath,savepath)
     imnum = imnum + 1
     print("Rotated picture saved: ", filename)
     
 print("imnunm: ", imnum)
+
+print('All Files Renamed')
+
+print('New Names are')
+# verify the result
+res = os.listdir(dirname)
+print(res)
