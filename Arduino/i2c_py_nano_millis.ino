9ac4ea932c52d40900dda89be4f02f2d12f80725
SN00R <2097noor@gmail.com>
2023-11-13 16:02:57 +0100

    arduino files

---
+++ Arduino/i2c_py_nano_millis.ino
@@ -0,0 +1,50 @@
+// Wire Master Reader
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Reads data from an I2C/TWI slave device
+// Refer to the "Wire Slave Sender" example for use with this
+
+// Created 29 March 2005
+
+// This example code is in the public domain.
+
+
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 lightMeter1(0x23);
+BH1750 lightMeter2(0x5C);
+long int t1, t2;
+
+unsigned long previousMillis = 0; 
+const long interval = 2000; 
+
+void setup()
+{
+  Serial.begin(9600);  // start serial for output
+  while (!Serial) delay(10); 
+  Wire.begin();        // join i2c bus (address optional for master)
+
+  lightMeter1.begin(BH1750::CONTINUOUS_HIGH_RES_MODE, 0x23, &Wire);
+  lightMeter2.begin(BH1750::CONTINUOUS_HIGH_RES_MODE, 0x5C, &Wire);
+  //Serial.println(F("BH1750 Test 2 Sensors"));
+  delay(5000);
+  //t1 = millis();
+}
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) 
+  {
+    //Serial.print("time in s: "); Serial.println(currentMillis);
+    previousMillis = currentMillis;
+    float lux1 = lightMeter1.readLightLevel();              // BH1750 0x23 Measurements
+    float lux2 = lightMeter2.readLightLevel();              // BH1750 0x5A Measurements
+    Serial.print(lux1); Serial.print(",");
+    Serial.print(lux2); Serial.println(",");
+  }
+
+}
+
---
+++ Arduino/i2c_py_zero_millis.ino
@@ -0,0 +1,57 @@
+// Wire Master Reader
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Reads data from an I2C/TWI slave device
+// Refer to the "Wire Slave Sender" example for use with this
+
+// Created 29 March 2005
+
+// This example code is in the public domain.
+
+
+#include <Wire.h>
+#include <Adafruit_SCD30.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_SCD30  scd30;
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+long int t1, t2;
+unsigned long previousMillis = 0; 
+const long interval = 2000; 
+const long setupinterval = 5000; 
+void setup()
+{
+  //unsigned long currentMillis = millis();
+  Serial.begin(9600);  // start serial for output
+  while (!Serial) delay(10);
+  Wire.begin();        // join i2c bus (address optional for master)
+  mlx.begin();
+  scd30.begin();
+  delay(6000);  
+  //if (millis() >= setupinterval)
+  {
+    //continue;
+  }
+}
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    previousMillis = currentMillis;
+    if (scd30.dataReady()){                                 // SCD30 Measurements
+      if (!scd30.read()){ return; }
+      Serial.print(mlx.readAmbientTempC()); Serial.print(",");       // MLX Measurements 
+      Serial.print(mlx.readObjectTempC()); Serial.print(",");   
+      Serial.print(scd30.temperature); Serial.print(",");
+      Serial.print(scd30.relative_humidity); Serial.print(",");
+      Serial.print(scd30.CO2, 3); Serial.print(",");
+      Serial.println(currentMillis);
+    } else {
+        Serial.println("NaN");
+    } 
+  }
+
+}
