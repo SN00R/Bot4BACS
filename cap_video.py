f0e7278f3e66747686e66cfe621f8b6e42ddb2e3
SN00R <2097noor@gmail.com>
2023-12-06 15:22:24 +0100

    adjust video capture script

--- cap_video.py
+++ cap_video.py
@@ -1,7 +1,10 @@
 import cv2
+imW = 1280
+imH = 720
 
 cap= cv2.VideoCapture(4)
-
+ret = cap.set(3, imW)
+ret = cap.set(4, imH)
 width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
@@ -10,10 +13,11 @@ writer= cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (
 
 while True:
     ret,frame= cap.read()
+    new = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
 
-    writer.write(frame)
+    writer.write(new)
 
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', new)
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
