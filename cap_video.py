cdcfe37681204c276aa06be25ee5d94da5c81203
SN00R <2097noor@gmail.com>
2023-12-06 15:42:05 +0100

    final capture video

--- cap_video.py
+++ cap_video.py
@@ -1,23 +1,20 @@
 import cv2
-imW = 1280
-imH = 720
 
-cap= cv2.VideoCapture(4)
-ret = cap.set(3, imW)
-ret = cap.set(4, imH)
+cap= cv2.VideoCapture(0)
+
 width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-writer= cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width,height))
+writer= cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(*'MJPG'), 20, (width,height))
 
 
 while True:
     ret,frame= cap.read()
-    new = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
+    #new = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
 
-    writer.write(new)
+    writer.write(frame)
 
-    cv2.imshow('frame', new)
+    cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
